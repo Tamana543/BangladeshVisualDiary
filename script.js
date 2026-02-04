c4c06f1f8284b4bc9812bb179f37581d78cceddf
Tamana543 <tamanafarzami33@gmail.com>
2026-02-04 06:57:07 +0430

    Functionality switched to JSON

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 const imageContainer = document.getElementById('imageContainer')
 
+function oldMethodTask(){
 
-let end = 34
-for (let i = 2; i <= end ; i++) {
-   
-     const html = `
-     <div class="image">
-                    <img src="./default_images/img_${i}.jpg" alt="${i} image">
-               </div>
-     `
-   imageContainer.innerHTML += html
-   
+     let end = 34
+     for (let i = 2; i <= end ; i++) {
+        
+          const html = `
+          <div class="image">
+                         <img src="./default_images/img_${i}.jpg" alt="${i} image">
+                    </div>
+          `
+        imageContainer.innerHTML += html
+        
+     }
 }
+
+// Via Json
+fetch('./images.json').then(res=>{
+ return res.json()
+})
+.then(images=>{
+images.forEach((element,ind) => {
+
+           imageContainer.innerHTML +=  `
+        <div class="image">
+          <img src="./default_images/${element}" alt="image ${ind}">
+        </div> ` 
+});
+})
+.catch(err=>console.log(err))
