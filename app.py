8d5011e91a9dceadfcf073958101c30fc2d56adf
Tamana543 <tamanafarzami33@gmail.com>
2026-02-22 07:22:53 +0430

    application updated, start changing the main fuctionality

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 #DataBase Hundler
 import sqlite3 # main sql :)
-from flask import jsonify
 def init_db():
      with sqlite3.connect("database.db") as data_table :
           data_table.execute("""
@@ -20,24 +19,33 @@ def test_data():
         """)
 
 # Flask Hundler 
-from flask import Flask, render_template,request
+import os
+from flask import Flask, render_template,request,jsonify
 
 app = Flask(__name__) # import flask 
+
 
 @app.route("/") # to create the web application and if someone visited  the homepage /, run the function below
 def main():
      # return "Hello" # if you see this in http://127.0.0.1:5000 url, means everything is okay in backend 
      return render_template("index.html")
 
-
+#defines the directory on the server where all uploaded files will be stored
+Upload_folder = "static/default_images"
+app.config["Upload_folder"] = Upload_folder #make the specified upload path accessible throughout your Flask application's views and logic. (by storing it in app.config)
 @app.route("/api/photos") # in this route it connect to database and get photos 
-def photo_render():
+def photo_upload():
+     
+
+     # with no file follow this 
+     '''
      data = sqlite3.connect("database.db")
      # to make rows to be like dictionaries so that it can be used as a json 
      data.row_factory = sqlite3.Row
      photos = data.execute('SELECT * FROM photos').fetchall()
      data.close()
      return jsonify([dict(photo) for photo in photos]) # to check if everything is good visite http://127.0.0.1:5000/api/photos
+     '''
 
 
 @app.route("/api/photos", methods=['POST'])
