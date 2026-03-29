f44de8f05aa7edb94abb67877d09963bc9bcf0ca
Tamana543 <tamanafarzami33@gmail.com>
2026-03-29 14:08:15 +0430

    file updated

--- app.py
+++ app.py
@@ -27,9 +27,11 @@ if not os.path.exists(UPLOAD_FOLDER):
 
 # Email handler 
 
+
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587 
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_PORT'] = 465 
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True 
 app.config['MAIL_USERNAME'] = 'auw242106@auw.edu.bd'
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD') # check this if email not working
 app.config['MAIL_DEFAULT_SENDER'] = 'auw242106@auw.edu.bd'
@@ -48,37 +50,41 @@ def edit_req():
 
 @app.route("/api/delete_request", methods=['POST'])
 def delete_request():
-     data = request.get_json()
-     sender_name = data.get('sender')
-     image_name = data.get('imageName')
-     reason = data.get('reason')
-     # Email for edit 
-     msg = Message(
-                subject="Your photo fron E_visual gallery.",
-               recipients= ["auw242106@auw.edu.bd"]
-               )
-              
-          # The template loader 
-     msg.html = render_template(
-            "email_template.html", 
-            sender_name=sender_name, 
-            photo_description= f"Reason for deletion :{reason} ",
-            email_reason = "Delete this image from dataBase.."
-        )
-     
-     filepath = os.path.join(app.config["UPLOAD_FOLDER"], image_name)
-     if os.path.exists(filepath):
-          with open(filepath,"rb") as img :
-               msg.attach(image_name, "image/jpeg", img.read())
-     else : 
-          print(f"Warning: {image_name} not found in folder. Sending without image ")
      try:
-          mail.send(msg)
-          return jsonify({"message" : "Request sent successfully, it will take at most two working days to approve your reqest :)"}) , 200
-     except Exception as e :
-          print(f"Error : {e}")
-          return jsonify({"message": "Failed to send email "}), 500
-     
+          data = request.get_json()
+          sender_name = data.get('sender')
+          image_name = data.get('imageName')
+          reason = data.get('reason')
+          # Email for edit 
+          msg = Message(
+                    subject="Your photo fron E_visual gallery.",
+                    recipients= ["auw242106@auw.edu.bd"]
+                    )
+               
+               # The template loader 
+          msg.html = render_template(
+               "email_template.html", 
+               sender_name=sender_name, 
+               photo_description= f"Reason for deletion :{reason} ",
+               email_reason = "Delete this image from dataBase.."
+          )
+          
+          filepath = os.path.join(app.config["UPLOAD_FOLDER"], image_name)
+          if os.path.exists(filepath):
+               with open(filepath,"rb") as img :
+                    msg.attach(image_name, "image/jpeg", img.read())
+          else : 
+               print(f"Warning: {image_name} not found in folder. Sending without image ")
+          try:
+               mail.send(msg)
+               return jsonify({"message" : "Request sent successfully, it will take at most two working days to approve your reqest :)"}) , 200
+          except Exception as e :
+               print(f"Error : {e}")
+               return jsonify({"message": "Failed to send email "}), 500
+     except Exception as e:
+        print(f"MAIL ERROR: {e}")
+        #return JSON even if it fails
+        return jsonify({"error": "Mail server connection timed out. Please try again later."}), 504
 
 
 
