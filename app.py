aaedc0c7dd57039a68b122bb25f2f1fafb5c340c
Tamana543 <tamanafarzami33@gmail.com>
2026-03-30 11:40:34 +0430

    file updated

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def send_gallery_email(to_email, subject, html_content, attachment_path=None, at
           return False
      
      params = {
-          "from": "E-Visual Gallery <tamanafarzami33@gmail.com>",
+          "from": "E-Visual Gallery <onboarding@resend.dev>",
           "to" :[to_email],
           "subject" : subject,
           "html" : html_content
